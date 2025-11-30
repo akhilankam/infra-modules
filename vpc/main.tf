d1540bd476243f64f489247d6d70302dbc8896f4
Akhil Ankam <akhilankam4747@gmail.com>
2025-11-30 17:20:42 +0530

    added the rds sg

--- vpc/main.tf
+++ vpc/main.tf
@@ -20,3 +20,25 @@ resource "aws_subnet" "private" {
   vpc_id     = aws_vpc.main.id
   cidr_block = each.value
 }
+
+resource "aws_security_group" "db" {
+  name        = "${var.name}-db-sg"
+  description = "Allow DB access within VPC"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = aws_vpc.main.cidr_block
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = var.tags
+}
--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -9,3 +9,7 @@ output "public_subnets" {
 output "private_subnets" {
   value = [for s in aws_subnet.private : s.id]
 }
+
+output "database_sg_id" {
+  value = aws_security_group.db.id
+}
