0a11611f8bc967d7486c73ffd3963fafcd51c80a
Akhil Ankam <akhilankam4747@gmail.com>
2025-12-06 12:40:04 +0530

    addded the availability parameter

--- vpc/main.tf
+++ vpc/main.tf
@@ -5,6 +5,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public" {
   for_each = toset(var.public_subnets)
 
+  availability_zone       = each.value
   vpc_id                  = aws_vpc.main.id
   cidr_block              = each.value
   map_public_ip_on_launch = true
@@ -13,8 +14,9 @@ resource "aws_subnet" "public" {
 resource "aws_subnet" "private" {
   for_each = toset(var.private_subnets)
 
-  vpc_id     = aws_vpc.main.id
-  cidr_block = each.value
+  availability_zone = each.value
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = each.value
 }
 
 resource "aws_security_group" "db" {
